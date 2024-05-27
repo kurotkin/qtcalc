7929ca54180567971e523965dcdb0032f170f545
kurotkin <kurotkin@users.noreply.github.com>
2024-05-27 23:07:02 +0300

    Добавлена логика

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,41 +2,195 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
-    ui->setupUi(this);
+    ui-> setupUi(this);
+    ui->displayView->clear();
 
-    connect(ui -> button0, SIGNAL(clicked()), this, SLOT(showNumbers()));
-    connect(ui -> button1, SIGNAL(clicked()), this, SLOT(showNumbers()));
-    connect(ui -> button2, SIGNAL(clicked()), this, SLOT(showNumbers()));
-    connect(ui -> button3, SIGNAL(clicked()), this, SLOT(showNumbers()));
-    connect(ui -> button4, SIGNAL(clicked()), this, SLOT(showNumbers()));
-    connect(ui -> button5, SIGNAL(clicked()), this, SLOT(showNumbers()));
+    operatorClicked = false;
+    hasStoredNumber = false;
+
+    ui->numberGroup->connect(
+                ui->numberGroup,
+                SIGNAL(buttonClicked(QAbstractButton*)),
+                this,
+                SLOT(numberGroup_clicked(QAbstractButton*)));
+
+    ui->actionGroup->connect(
+                ui->actionGroup,
+                SIGNAL(buttonClicked(QAbstractButton*)),
+                this,
+                SLOT(actionGroup_clicked(QAbstractButton*)));
 }
 
 MainWindow::~MainWindow(){
     delete ui;
 }
 
-void MainWindow::showNumbers(){
-    QPushButton *button = static_cast<QPushButton*>(QObject::sender());
-    QString name = button->objectName();
+void MainWindow::numberGroupClicked(QAbstractButton* button){
+    QString displayLabel = ui->displayView->text();
 
-    int disp = ui -> lcdNumber -> value();
-    if(name == "button0"){
-        disp = disp * 10;
-    } else if (name == "button1") {
-        disp = disp * 10 + 1;
-    } else if (name == "button2") {
-        disp = disp * 10 + 2;
-    } else if (name == "button3") {
-        disp = disp * 10 + 3;
-    } else if (name == "button4") {
-        disp = disp * 10 + 4;
-    } else if (name == "button5") {
-        disp = disp * 10 + 5;
+    if (operatorClicked) {
+        displayLabel.clear();
+        operatorClicked = false;
     }
 
-    ui -> lcdNumber -> display(disp);
+    if (displayLabel.length() >= DIGIT_LIMIT) {
+        return;
+    }
+
+    displayLabel.append(button->text());
+    ui->displayView->setText(displayLabel);
 }
 
+void MainWindow::actionGroupClicked(QAbstractButton* button) {
+    if (operatorClicked) {
+        storedOperator = button->text().at(0);
+    } else {
+        if (hasStoredNumber) {
+            calcResult();
+        }
+        else {
+            hasStoredNumber = true;
+            QString displayLabel = ui->displayView->text();
+            storedNumber = displayLabel.toDouble();
+        }
+        operatorClicked = true;
+        storedOperator = button->text().at(0);
+    }
+}
+
+void MainWindow::onActionDelClicked(){
+    QString displayLabel = ui->displayView->text();
+    if (displayLabel.length() == 0) return;
+
+    displayLabel.QString::chop(1);
+    ui->displayView->setText(displayLabel);
+}
+
+void MainWindow::onActionCalcClicked(){
+    QString displayLabel = ui->displayView->text();
+    if (!hasStoredNumber || displayLabel.length() < 1 || operatorClicked) {
+        return;
+    }
+    calcResult();
+    hasStoredNumber = false;
+}
+
+void MainWindow::onCommaClicked(){
+    QString displayLabel = ui->displayView->text();
+    if (displayLabel.length() >= (DIGIT_LIMIT - 1) ||displayLabel.contains('.', Qt::CaseSensitive)) {
+        return;
+    }
+
+    if (displayLabel.length() == 0) {
+        displayLabel = "0";
+    }
+
+    displayLabel.append('.');
+    ui->displayView->setText(displayLabel);
+}
+
+void MainWindow::onActionClearClicked(){
+    ui->displayView->clear();
+    operatorClicked = false;
+    hasStoredNumber = false;
+}
+
+void MainWindow::onActionPercentClicked(){
+    QString displayLabel = ui->displayView->text();
+    displayLabel = QString::number(displayLabel.toDouble() * 0.01,'g', DIGIT_LIMIT);
+    ui->displayView->setText(displayLabel);
+}
+
+void MainWindow::onActionSignClicked(){
+    QString displayLabel = ui->displayView->text();
+    displayLabel = QString::number(displayLabel.toDouble() * -1,'g', DIGIT_LIMIT);
+    ui->displayView->setText(displayLabel);
+}
+
+void MainWindow::calcResult() {
+    QString displayLabel = ui->displayView->text();
+     if (displayLabel.endsWith('.',Qt::CaseSensitive)) {
+         displayLabel.QString::chop(1);
+     }
+
+     if (storedOperator == '+') {
+         storedNumber += displayLabel.toDouble();
+     }
+     else if (storedOperator == '-') {
+         storedNumber -= displayLabel.toDouble();
+     }
+     else if (storedOperator == 'x') {
+         storedNumber *= displayLabel.toDouble();
+     }
+     else if (storedOperator == '/') {
+         storedNumber /= displayLabel.toDouble();
+     }
+
+     displayLabel = QString::number(storedNumber,'g', DIGIT_LIMIT);
+     ui->displayView->setText(displayLabel);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e) {
+    switch (e->key()) {
+        case Qt::Key_1:
+            numberGroupClicked(ui->num1);
+            break;
+        case Qt::Key_2:
+            numberGroup_clicked(ui->num2);
+            break;
+        case Qt::Key_3:
+            numberGroup_clicked(ui->num3);
+            break;
+        case Qt::Key_4:
+            numberGroup_clicked(ui->num4);
+            break;
+        case Qt::Key_5:
+            numberGroup_clicked(ui->num5);
+            break;
+        case Qt::Key_6:
+            numberGroup_clicked(ui->num6);
+            break;
+        case Qt::Key_7:
+            numberGroup_clicked(ui->num7);
+            break;
+        case Qt::Key_8:
+            numberGroup_clicked(ui->num8);
+            break;
+        case Qt::Key_9:
+            numberGroup_clicked(ui->num9);
+            break;
+        case Qt::Key_0:
+            numberGroup_clicked(ui->num0);
+            break;
+        case Qt::Key_Plus:
+            actionGroup_clicked(ui->actionPlus);
+            break;
+        case Qt::Key_Minus:
+            actionGroup_clicked(ui->actionMinus);
+            break;
+        case Qt::Key_Asterisk:
+            actionGroup_clicked(ui->actionMul);
+            break;
+        case Qt::Key_Slash:
+            actionGroup_clicked(ui->actionDiv);
+            break;
+        case Qt::Key_Period:
+            onCommaClicked();
+            break;
+        case Qt::Key_Enter:
+        case Qt::Key_Return:
+            onActionCalcClicked();
+            break;
+        case Qt::Key_Backspace:
+            onActionDelClicked();
+            break;
+        case Qt::Key_Delete:
+            onActionClearClicked();
+            break;
+        case Qt::Key_Percent:
+            onActionPercentClicked();
+            break;
+    }
+}
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -18,9 +18,31 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    const int DIGIT_LIMIT = 16;
+    //Flag to check whether the previous button that was clicked was an operator
+    bool operatorClicked;
+    //Last operator requested
+    QChar storedOperator;
+    //Flag to check whether a number is stored in memory
+    bool hasStoredNumber;
+    //Stored number
+    double storedNumber;
+    //Calculate result based on stored number and displayed number
+    void calcResult();
 
 public slots:
-    void showNumbers();
+    void numberGroupClicked(QAbstractButton*);
+    void actionGroupClicked(QAbstractButton*);
+
+    void onActionDelClicked();
+    void onActionCalcClicked();
+    void onCommaClicked();
+    void onActionClearClicked();
+    void onActionPercentClicked();
+    void onActionSignClicked();
+
+protected:
+    void keyPressEvent(QKeyEvent *e);
 
 };
 #endif // MAINWINDOW_H
